4cf9eec17fa0524ac5926e39c69d4097dd73bc41
vid podpecan <vid.podpecan@ijs.si>
2020-03-30 22:06:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+### A parser and exporter for Croatian web corpus hrWaC 2.1
+
+#### About
+This python3 script reads, filters and exports the hrWaC 2.1 corpus from `.xml.gz` archives, either from a local copy or directly from the [CLARIN.SI](https://www.clarin.si/repository/xmlui/handle/11356/1064) repository.
+
+The goal is to extract a large, high quality training set to train word and document embeddings such as `word2vec` and `doc2vec`.
+
+Paragraphs are token-filtered according to the parameter settings and exported into a [TaggedLineDocument](https://radimrehurek.com/gensim/models/doc2vec.html#gensim.models.doc2vec.TaggedLineDocument) file where each line contains tokens from one paragraph.
+
+The `read_corpus` function can be also used as an iterator over paragraph tokens from the selected hrWaC `.xml.gz` archive.
+
+
+#### Requirements
+The code works with `python 3.4+` and requires the following python modules (see requirements.txt):
+
+-  `gensim`
+-  `smart_open`
+-  `langdetect`
+
+#### How to use
+
+The following command will extract paragraph tokens directly from the [CLARIN.SI](https://www.clarin.si/repository/xmlui/handle/11356/1064) hrWaC 2.1 repository using default parameter values and save them into a `hrwac.tld` file:
+
+```sh
+python3 process_hrwac.py --web hrwac.tld
+```
+
+There is also the `--detect_language` parameter which will instruct the parser to ignore all paragraphs that are not in Croatian. This will make the export **considerably slower** but is recommended for a high quality output because hrWaC contains a small portion of paragraphs in other languages (Slovene, English, ...).  
+
+The parser supports the following parameters:
+
+-  `-f`, `--folder`: input folder with hrWaC .xml.gz files
+-  `-m`, `--min_length`, default=50, minimum paragraph length (in tokens)
+-  `-M`, `--max_length`, default=unlimited, maximum paragraph length (in tokens)
+-  `-D`, `--max_docs`, default=unlimited, maximum number of documents from each archive, useful to extract small samples
+-  `--detect_language`, default: false, detect language and filter non `hr` paragraphs (much slower!)
+-  `--web`, read corpus directly from the CLARIN.SI repository
+-  `output_file`, name of the output file (a positional argument)
+
+Note that the `--folder` and `--web` parameters are mutually exclusive.
+
+
+#### Author
+Vid Podpečan <vid.podpecan@ijs.si>
+
+
+#### License
+
+MIT
---
+++ process_hrwac.py
@@ -0,0 +1,114 @@
+import argparse
+import xml
+import csv
+import io
+import time
+import os
+import sys
+import gensim
+from gensim.test.utils import datapath
+from gensim.models.doc2vec import TaggedLineDocument
+import smart_open
+from langdetect import detect
+from langdetect import DetectorFactory
+DetectorFactory.seed = 123
+
+IGNORE_TOKENS = ['(', ')', '[', ']', '{', '}', "'", '"', '»', '«', ';', ':', ',', '!', '/', '\\',
+                 '@', '#', '$', '^', '*', '+', '=', '-', '|', '_', '<', '>', '?', '!',
+                 '.', ',', '&', '%', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                 '..', '...', '....', '.....']
+
+
+def filter_tokens(tokens, ignorelist=IGNORE_TOKENS, minlen=2, maxlen=20, is_printable=True, is_alphanumeric=True, allow_numbers=False):
+    result = [t.strip() for t in tokens if t not in ignorelist and len(t) >= minlen and len(t) <= maxlen]
+    if is_printable:
+        result = filter(str.isprintable, result)
+        # result = [t for t in result if t.isprintable()]
+    if is_alphanumeric:
+        result = filter(str.isalnum, result)
+        # result = [t for t in result if t.isalnum()]
+    if not allow_numbers:
+        result = [t for t in result if not t.replace('.', '').replace(',', '').replace(':', '').isdigit()]  # catch also real numbers and time measurements
+    return result
+
+
+def read_corpus(filename, tokens_only=False, max_docs=None, min_tokens_per_doc=10, max_tokens_per_doc=None,
+                detect_language=True):
+    cnt = 0
+    with smart_open.open(filename) as fp:
+        for event, elt in xml.etree.ElementTree.iterparse(fp, ['end']):
+            if elt.tag == 'p':
+                par_tokens = []
+                for text in elt.itertext():
+                    tokens = [row[1].lower() for row in csv.reader(io.StringIO(text), delimiter='\t') if len(row) == 4]
+                    par_tokens.extend(filter_tokens(tokens))
+
+                # filter too short and too long
+                if len(par_tokens) < min_tokens_per_doc or (max_tokens_per_doc is not None and len(par_tokens) > max_tokens_per_doc):
+                    continue
+
+                # try to detect language
+                if detect_language:
+                    try:
+                        par_text = ' '.join(par_tokens)
+                        if detect(par_text) != 'hr':
+                            continue
+                    except Exception as e:
+                        pass
+
+                # finally, yield tokens or tagged document
+                cnt += 1
+                if max_docs is not None and cnt > max_docs:
+                    return
+                if tokens_only:
+                    yield par_tokens
+                else:
+                    yield gensim.models.doc2vec.TaggedDocument(par_tokens, [cnt])
+
+
+def to_taglndoc(archives, outfile, **kwargs):
+    total_count = 0
+    with open(outfile, 'w', encoding='utf-8') as ofp:
+        for archive in archives:
+            print('Processing {} ...'.format(archive), end=' ', file=sys.stderr)
+            cnt = 0
+            for doc in read_corpus(archive, **kwargs):
+                line = ' '.join(doc.words) + '\n'
+                ofp.write(line)
+                cnt += 1
+            total_count += cnt
+            print('{} paragraphs exported.'.format(cnt), file=sys.stderr)
+    print('Summary: {} paragraphs exported.'.format(total_count), file=sys.stderr)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Read hrWaC2.1 .xml.gz archive and write a TaggedLineDocument output')
+    parser.add_argument('-f', '--folder', help='input folder with hrWaC .xml.gz files')
+    parser.add_argument('-m', '--min_length', default=50, type=int, help='minimum paragraph length (in tokens); default: 50')
+    parser.add_argument('-M', '--max_length', default=None, type=int, help='maximum paragraph length (in tokens); default: unlimited')
+    parser.add_argument('-D', '--max_docs', default=None, type=int, help='maximum number of documents from each archive; default: unlimited')
+    parser.add_argument('--detect_language', action='store_true', help='detect language and filter non "hr" paragraphs (much slower!)')
+    parser.add_argument('--web', action='store_true', help='read corpus directly from clarin.si repository')
+    parser.add_argument('outfile', help='output file')
+    args = parser.parse_args()
+
+    if args.web and args.folder:
+        print('--folder and --web parameters are mutually exclusive, please set only one')
+        exit(1)
+    if args.web:
+        base = 'https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1064'
+        archives = ['{}/hrWaC2.1.{:02d}.xml.gz'.format(base, i) for i in range(1,15)]
+    elif args.folder:
+        archives = sorted([entry.path for entry in os.scandir(args.folder) if entry.path.lower().endswith('.xml.gz')])
+        if len(archives) == 0:
+            print('No .xml.gz archives in folder "{}"!'.format(args.folder))
+            exit(1)
+    else:
+        print('One of these two arguments [folder, web] is required!')
+        exit(1)
+
+    to_taglndoc(archives, args.outfile,
+                min_tokens_per_doc=args.min_length,
+                max_tokens_per_doc=args.max_length,
+                max_docs=args.max_docs,
+                detect_language=args.detect_language)
